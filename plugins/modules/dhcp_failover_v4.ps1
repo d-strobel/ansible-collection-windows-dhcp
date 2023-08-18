23f06eee2fd83e9923860c433d5c4feb49100bdd
Dustin Strobel <ngvs_cacophonous@aleeas.com>
2023-08-18 16:48:47 +0200

    fix: Change comparison

--- plugins/modules/dhcp_failover_v4.ps1
+++ plugins/modules/dhcp_failover_v4.ps1
@@ -8,14 +8,15 @@
 
 $spec = @{
     options             = @{
-        name                = @{ type = "str"; required = $true }
-        partner_server      = @{ type = "str"; required = $true }
-        scopes              = @{ type = "list" ; elements = "str"; required = $true }
-        shared_secret       = @{ type = "str" }
-        mode                = @{ type = "str"; choices = "loadbalance", "hotstandby"; default = "loadbalance" }
-        server_role         = @{ type = "str"; choices = "active", "standby" }
-        loadbalance_percent = @{ type = "int"; default = 50 }
-        state               = @{ type = "str"; choices = "absent", "present"; default = "present" }
+        name                 = @{ type = "str"; required = $true }
+        partner_server       = @{ type = "str"; required = $true }
+        scopes               = @{ type = "list" ; elements = "str"; required = $true }
+        shared_secret        = @{ type = "str" }
+        shared_secret_update = @{ type = "str"; choices = "always", "never"; default = "never" }
+        mode                 = @{ type = "str"; choices = "loadbalance", "hotstandby"; default = "loadbalance" }
+        server_role          = @{ type = "str"; choices = "active", "standby" }
+        loadbalance_percent  = @{ type = "int"; default = 50 }
+        state                = @{ type = "str"; choices = "absent", "present"; default = "present" }
     }
 
     required_if         = @(
@@ -33,6 +34,7 @@ $name = $module.Params.name
 $partnerServer = $module.Params.partner_server
 $scopeId = $module.Params.scopes
 $sharedSecret = $module.Params.shared_secret
+$sharedSecretUpdate = $module.Params.shared_secret_update
 $mode = $module.Params.mode
 $serverRole = $module.Params.server_role
 $loadbalancePercent = $module.Params.loadbalance_percent
@@ -109,31 +111,41 @@ if (($null -eq $dhcpServersFailover) -and ($state -eq "present")) {
     }
 }
 
-# # Compare changes
-# if (
-#     ($dhcpServersScope.StartRange -ne $startRange) -or
-#     ($dhcpServersScope.EndRange -ne $endRange) -or
-#     ($dhcpServersScope.Name.Trim() -ne $name) -or
-#     ($dhcpServersScope.Description.Trim() -ne $description) -or
-#     ($dhcpServersScope.Type -ne $type) -or
-#     ($dhcpServersScope.State -ne $scopeState)
-# ) {
-#     try {
-#         Set-DhcpServerv4Scope `
-#             -ScopeId $scopeId `
-#             -StartRange $startRange `
-#             -EndRange $endRange `
-#             -Name $name`
-#             -Description $description`
-#             -State $scopeState `
-#             -Type $type `
-#             -Confirm:$false
+# Compare failover mode
+if ($dhcpServersFailover.Mode -ne $mode) {
+    try {
+        Set-DhcpServerv4Failover -Name $name -Mode $mode -Confirm:$false
+        $module.Result.changed = $true
+    }
+    catch {
+        $module.FailJson("Failed to set failover mode to '$mode'", $Error[0])
+    }
+}
 
-#         $module.Result.changed = $true
-#     }
-#     catch {
-#         $module.FailJson("Failed to set changed parameters for scope '$scopeId'", $Error[0])
-#     }
-# }
+# Update shared secret
+if ($sharedSecretUpdate -eq "always") {
+    try {
+        Set-DhcpServerv4Failover -Name $name -SharedSecret $sharedSecret -Confirm:$false -Force
+        $module.Result.changed = $true
+    }
+    catch {
+        $module.FailJson("Failed to set shared secret", $Error[0])
+    }
+}
+
+# Compare loadbalance parameters
+if (
+    ($dhcpServersFailover.Mode -eq "loadbalance") -and
+    ($dhcpServersFailover.LoadbalancePercent -ne $loadbalancePercent)
+) {
+    try {
+        Set-DhcpServerv4Failover -Name $name -LoadBalancePercent $loadbalancePercent -Confirm:$false
+        $module.Result.changed = $true
+    }
+    catch {
+        $module.FailJson("Failed to set loadbalance_percent to $loadbalancePercent %", $Error[0])
+    }
+}
+
 
 $module.ExitJson()
